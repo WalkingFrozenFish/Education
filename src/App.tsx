bec75f6b6d4744075048b914d7b70825e287f1e0
Oleg Chachin <chachin.ol@yandex.kz>
2023-08-12 03:06:42 +0600

    Add card text ellipsis

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import Card from "./components/Card.tsx";
 function App() {
   return (
     <>
-      <Card url={"https://fakeimg.pl/350x200/?text=Music is good"} title={"Some music theory"} author={"Music" +
+      <Card url={"https://fakeimg.pl/350x200/?text=Music is good"} title={"Some music theory and very loooooooong" +
+          " title for course looooool"} author={"Music" +
           " academy"} type={"Art/Music"} newPrice={"100"} oldPrice={""} reviewCount={"234"} />
       <Card url={"https://fakeimg.pl/350x200/?text=CS"} title={"Computer science for start"} author={"Harvard"} type={"Computer science"} newPrice={"522"} oldPrice={"100"} reviewCount={"5124"} />
       <Card url={"https://fakeimg.pl/350x200/?text=Bio"} title={"Biotechnology introducing"} author={"Institute bio" +
